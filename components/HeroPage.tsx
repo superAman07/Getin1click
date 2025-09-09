a3e50ddd27910adf63c42854721f2453a4a3cc6a
superAman07 <rivalaman123@gmail.com>
2025-09-09 16:07:37 +0530

    changed its name

---
+++ components/HeroPage.tsx
@@ -0,0 +1,7 @@
+export default function (){
+    return (
+        <div className="flex justify-center items-center h-screen text-3xl font-bold">
+            hello from Hero Page
+        </div>
+    )
+}
