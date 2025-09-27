7b571d018de3ee5c0b1e3f791b48b238a0720525
superAman07 <rivalaman123@gmail.com>
2025-09-27 12:04:34 +0530

    anijaso

--- middleware.ts
+++ middleware.ts
@@ -5,32 +5,42 @@ export async function middleware(req: NextRequest) {
     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
     const { pathname } = req.nextUrl;
 
-    if (token && pathname.startsWith('/auth')) {
-        return NextResponse.redirect(new URL('/', req.url));
+    const isAdmin = token?.role === 'ADMIN';
+    const isAuthPage = pathname.startsWith('/auth');
+    const isAdminDashboard = pathname.startsWith('/dashboard');
+
+    if (isAdmin) {
+        if (!isAdminDashboard) {
+            return NextResponse.redirect(new URL('/dashboard', req.url));
+        }
+    } 
+    
+    else if (token && !isAdmin) {
+        if (isAuthPage) {
+            return NextResponse.redirect(new URL('/', req.url));
+        }
+        if (isAdminDashboard) {
+            return NextResponse.redirect(new URL('/', req.url));
+        }
     }
-
-    //   const isProtectedRoute = !pathname.startsWith('/auth') && !pathname.startsWith('/api') && pathname !== '/favicon.ico';
-
-    //   if (!token && isProtectedRoute) {
-    //     return NextResponse.redirect(new URL('/auth/login', req.url));
-    //   }
+    else if (!token) {
+        if (isAdminDashboard) {
+            return NextResponse.redirect(new URL('/auth/login', req.url));
+        }
+    }
 
     return NextResponse.next();
 }
 
-// This config specifies which paths the middleware should run on.
-// We are essentially protecting everything except for the auth page itself
-// when the user is not logged in.
 export const config = {
-    //   matcher: [
-    //     /*
-    //      * Match all request paths except for the ones starting with:
-    //      * - _next/static (static files)
-    //      * - _next/image (image optimization files)
-    //      * - favicon.ico (favicon file)
-    //      * We want the middleware to run on the auth page to handle the redirect for logged-in users.
-    //      */
-    //     '/((?!_next/static|_next/image).*)',
-    //   ],
-    matcher: ['/auth/:path*'],
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+        '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    ],
 };
