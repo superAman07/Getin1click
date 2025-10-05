ddc8448c8699ead26d2598779c4d9efd9b633828
superAman07 <rivalaman123@gmail.com>
2025-10-05 19:05:13 +0530

    sdji

--- app/api/professional/profile/route.ts
+++ app/api/professional/profile/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import prisma from '@/lib/db';
